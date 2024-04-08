197d7e4b5d7014f41bb68c312c99600d8d76c368
Olli Niemitalo <olli4@hamk.fi>
2024-04-08 16:01:44 +0300

    full oConfig.siteSelectorMapview and oConfig.siteMapView

--- assets/js/ChildConfig.js
+++ assets/js/ChildConfig.js
@@ -4,6 +4,15 @@ var childOConfig = {
     storageUrl: '', // FILL!
     sitesGeojsonFilename: 'sites.geojson',
     blocksGeojsonFilename: 'blocks.geojson',
+    /**
+     * Configuration object for the site selector map view.
+     * Documentarion for the options Object passed to Map constructor applies: https://docs.mapbox.com/mapbox-gl-js/api/map/
+     * @type {Object}
+     * @property {Object} fitBoundsOptions - Options for fitting the map bounds.
+     * @property {number} fitBoundsOptions.padding - Padding value for fitting the map bounds.
+     * @property {number} minZoom - Minimum zoom level for the map.
+     * @property {number} maxZoom - Maximum zoom level for the map.
+     */
     siteSelectorMapView : {
         fitBoundsOptions: {
             padding: 40,
@@ -11,6 +20,11 @@ var childOConfig = {
         minZoom: 3,
         maxZoom: 10,
     },
+    /**
+     * The site map view configuration.
+     * Documentarion for the options Object passed to Map constructor applies: https://docs.mapbox.com/mapbox-gl-js/api/map/
+     * @type {SiteMapView}
+     */
     siteMapView : {
         minZoom: 10,
         maxZoom: 17,
